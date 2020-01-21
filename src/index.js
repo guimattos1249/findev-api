07885df20ae71c01e03c8d2b9ebdf879d5143dcc
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-21 20:50:43 -0300

    Adding the cors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://findev-api:findev-api@cluster0-vw5cf.mongodb.net
     useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
