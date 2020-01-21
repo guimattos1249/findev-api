c1760df947067bf712b6be3cd025cd8c406b7940
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-21 00:27:08 -0300

    Creating routes do list Devs

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,13 @@
 const { Router } = require('express');
-const axios = require('axios');
-const Dev = require('./models/Dev');
+
+const DevController = require('./controllers/DevController');
+const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.post('/devs', async (req, res) => {
-    const { github_username, techs, latitude, longitude } = req.body;
+routes.get('./devs', DevController.index);
+routes.post('/devs', DevController.store);
 
-    const apiResponse =  await axios.get(`https://api.github.com/users/${github_username}`);
-
-    const {name = login, avatar_url, bio} = apiResponse.data;
-
-    const techsArray = techs.split(',').map(tech => tech.trim());
-
-    const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude]
-    }
-
-    const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location
-    });
-
-    return res.json({dev});
-});
+routes.get('/search', SearchController.index);
 
 module.exports = routes;
