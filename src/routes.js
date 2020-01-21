ce79339530684833ccce5e91fe1622d344e8e228
Guilherme Mattos <guilhermevsmattos@outlook.com>
2020-01-21 00:30:59 -0300

    Fix in route get '/devs'

--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const SearchController = require('./controllers/SearchController');
 
 const routes = Router();
 
-routes.get('./devs', DevController.index);
+routes.get('/devs', DevController.index);
 routes.post('/devs', DevController.store);
 
 routes.get('/search', SearchController.index);
